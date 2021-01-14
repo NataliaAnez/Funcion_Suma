9cf048d27f381a9a47fcb35188a77df7be7669db
Natalia Anez Safar <nataliaanezsafar@gmail.com>
2021-01-13 20:36:49 -0400

    Suma con get.

---
+++ Solución_Suma/WEB suma/Controllers/Suma Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_suma.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Suma_Controller : Controller
+    {
+        [HttpGet]
+
+        public int Suma(int num1, int num2)
+        {
+
+        return num1 + num2;
+
+        }
+
+    }
+}
